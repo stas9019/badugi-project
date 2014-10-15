dd3a8e8f1b2d1b9726f87c1a3a1bf4e9503bfe24
staszamana95@gmail.com <staszamana95@gmail.com@f38eb0c4-25cb-76d6-1da2-59e377c92437>
2014-10-15 11:28:51 +0000

    ClientWorker, Connection and Game Frame prototypes added.

---
+++ badugi-project/badugi/src/Client/ConnectionFrame.java
@@ -0,0 +1,27 @@
+package Client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/*
+ * TO DO 
+ * Client's GUI: First Frame - connection frame, Second - Game Frame
+ * Establishing connection by IP and port
+ */
+
+public class ConnectionFrame extends JFrame implements ActionListener
+{
+
+	ConnectionFrame(Client client)
+	{
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
