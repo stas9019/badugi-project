19095593e09a22cfab8759f1b827636a5baea9bc
joanna.kowalska.ja@gmail.com <joanna.kowalska.ja@gmail.com@f38eb0c4-25cb-76d6-1da2-59e377c92437>
2014-10-30 18:14:36 +0000

    Lastest version of proposition of GameFrame look.

--- trunk/badugi-project/badugi/src/Client/GameFrame2.java
+++ trunk/badugi-project/badugi/src/Client/GameFrame2.java
@@ -8,16 +8,27 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+	/*
+	 * To Add:
+	 * Dealer/smallBlind/bigBlind
+	 * Status
+	 * change button + checkBoxes
+	 * time left label
+	 */
+
 public class GameFrame2 extends JFrame
 {
 	//private Client client;
-	private JButton bConfirm, bBet, bCall, bRaise, bAllIn, bCheck, bFold;
+	private JButton bConfirm, bBet, bCall, bRaise, bAllIn, bCheck, bFold, bChange;
 	private JLabel lMoney, lPot;
+	private JButton cards[] = new JButton[4];
+	private JCheckBox boxes[] = new JCheckBox[4];
 	private JTextField tBid;
 
 	GameFrame2(/*Client client*/)
@@ -33,7 +44,7 @@ public class GameFrame2 extends JFrame
 		setLayout(null);
 
 		try {
-			setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/back.jpg")))));
+			setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/Client/back.jpg")))));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -43,7 +54,7 @@ public class GameFrame2 extends JFrame
 		lMoney.setBackground(new Color(250,250,250));
 		lMoney.setOpaque(true);
 		add(lMoney);
-		
+				
 		lPot = new JLabel("  Current pot: ");
 		lPot.setBounds(235, 285, 205, 20);
 		lPot.setBackground(new Color(250,250,250));
@@ -72,7 +83,7 @@ public class GameFrame2 extends JFrame
 		bRaise.setBounds(235, 310, 100, 40);
 		add(bRaise);
 		
-		bAllIn = new JButton("AllIn");
+		bAllIn = new JButton("All In");
 		bAllIn.setBounds(340, 310, 100, 40);
 		add(bAllIn);
 		
@@ -83,6 +94,24 @@ public class GameFrame2 extends JFrame
 		bFold = new JButton("Fold");
 		bFold.setBounds(550, 310, 100, 40);
 		add(bFold);
+		
+		for(int i = 0; i < 4 ;i++) {
+			cards[i] = new JButton("Card" +i);
+			cards[i].setBounds(150+(i*95), 140, 90, 130);
+			add(cards[i]);
+		}
+
+		for(int i = 0; i < 4 ;i++) {
+			boxes[i] = new JCheckBox("");
+			boxes[i].setBounds(185+(i*95), 120, 20, 15);
+			boxes[i].setOpaque(false); //transparent background behind checkboxes xD
+			add(boxes[i]);
+		}
+		
+		bChange = new JButton("<html><center>Change<br/>cards</center></html>");
+		bChange.setBounds(550, 230, 100, 40);
+		add(bChange);
+		
 
 		setVisible(true);
 
