ae7aac933c4a2667b9b312c458f71c38589e73aa
staszamana95@gmail.com <staszamana95@gmail.com@f38eb0c4-25cb-76d6-1da2-59e377c92437>
2014-10-30 15:02:00 +0000

    29.10 version

---
+++ trunk/badugi-project/badugi/branch1/branch/src/badugi/Server.java
@@ -0,0 +1,127 @@
+package badugi;
+
+
+import java.io.*;
+import java.net.*;
+
+import Client.Client;
+
+/*
+ * Segregate Server and ClientWorker work
+ * Game class makes general changes, ClientWorker only players needs
+ * Make smth like in Wolf-Hare game, to allow players make decisions step-by-step
+ * 
+ */
+public class Server 
+{
+
+	private int port;
+	private int money;
+	private int playersNumber;
+	
+	private int playersCounter = 0;
+	private boolean init;
+	private ServerSocket socket;
+	private ClientWorker players[];  
+	private Game game;
+	private PrintWriter out;
+	
+	Server(int playersNumber, int money, int port)
+	{
+		
+		this.port = port;
+		this.money = money;
+		this.playersNumber = playersNumber;
+		
+		init = true;
+		
+		players = new ClientWorker[playersNumber]; //1 more, because server makes worker[0]	
+
+	}
+	
+	public void listenSocket() 
+	{
+		try 
+		{
+	    	socket = new ServerSocket(port); 
+	    } 
+		catch (IOException e) 
+		{
+			System.out.println("Could not listen on port " + port); 
+			//System.exit(-1);
+		}
+		while(true)
+		{
+		    
+			
+			
+			if(playersCounter < playersNumber )
+			{
+			
+				try
+				{
+					players[playersCounter] = new ClientWorker(socket.accept(), game);
+					System.out.println(playersCounter);
+					
+					Thread t = new Thread(players[playersCounter]);
+					playersCounter++;
+					t.start();
+				   	    
+				} 
+				catch (IOException e) 
+				{
+					System.out.println("Acception failed");
+					//System.exit(-1);
+				}
+				
+				if(playersCounter  == playersNumber )
+				{
+					System.out.println("Game starts, Server");
+					game = new Game(players, money);
+				}
+			}
+			else
+			{	
+				try
+				{	
+					out = new PrintWriter(socket.accept().getOutputStream(), true);
+					out.println("Sorry, game already started");
+				}
+				catch(IOException e) 
+				{
+					System.out.println("Acception failed");
+				}
+			}
+		}
+	}
+	
+	/*
+	 * args[0] - Number of players
+	 * args[1] - Start sum for each player
+	 * args[2] - Server port
+	 */
+	public static void main(String[] args) 
+	{
+		
+		int playersNumber = 0;
+		int money = 0;
+		int serverPort = 0;
+		
+		try
+		{
+			playersNumber = Integer.parseInt(args[0]);
+			money = Integer.parseInt(args[1]);
+			serverPort = Integer.parseInt(args[2]);
+			
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("");	
+		}
+		
+		Server serv = new Server(playersNumber, money, serverPort);
+		serv.listenSocket();
+
+	}
+
+}
---
+++ trunk/badugi-project/badugi/branch1/src/Client/ClientWorker.java
@@ -0,0 +1,145 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import badugi.Game;
+
+/*TO-DO
+ * Finalize method
+ * */
+
+
+public class ClientWorker implements Runnable
+{
+	
+	private Socket socket = null;
+	private PrintWriter out = null;
+	private BufferedReader in = null;
+    private Client client;
+    private ConnectionFrame connectionFrame;
+    private String adress;
+    private int port;
+    
+	ClientWorker(Client client, Socket socket) //was Server server
+	{
+		this.socket=socket;
+		this.client = client;
+		
+		connectionFrame = client.getConnectionFrame();
+	}
+	
+	public void sayToClient(String answer)
+	{
+		
+		out.println(answer);
+		System.out.println("Client worker " +answer);
+	}
+	
+	
+	public void listenSocket() throws NullPointerException
+	{
+		
+	        try 
+	        {
+	        	out = new PrintWriter(socket.getOutputStream(), true);
+	        	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	          
+	        	//System.out.println("Connected on " + socket.getPort());
+	        }
+	        catch(NullPointerException e)
+	        {
+	        	connectionFrame.setOutputText("No connection");
+	        }
+	        catch(NumberFormatException e) 
+	        {
+	        	connectionFrame.setOutputText("NumberFormatException"); 
+	        }
+	        catch (UnknownHostException e) 
+	        {
+	        	connectionFrame.setOutputText("Unknown host: "+ adress); 
+	        }
+	        catch  (IOException e) 
+	        {
+	        	connectionFrame.setOutputText("No I/O"); 
+	        }
+	        catch (IllegalArgumentException e) 
+	        {
+	        	connectionFrame.setOutputText("Wrong port: "+ port); 
+	        }
+	        
+	        String text = "";
+	        
+	        while( text != null )
+			{
+	        	
+		        try
+				{
+					text = in.readLine();					
+				} 
+	        	
+	        	catch (IOException e)
+				{
+	        		connectionFrame.setOutputText("No I/O"); 
+				}
+		        
+		        /*server's answer analyze part
+		         * maybe some special function for this*/
+		        
+		        if(text.equals("Connected!"))
+		        {
+		        	connectionFrame.blockConnectButton();
+		        	connectionFrame.setOutputText("Connected!");		        	
+		        }
+		        
+		        if(text.equals("Game starts!"))
+		        {
+		        	connectionFrame.setVisible(false);
+		        	
+		        	client.invokeGameFrame();
+		        	client.initSuit();	        	
+		        }
+		        if(text.equals("First card distribution"))
+		        {
+		        	client.getCards();
+		        }
+		      //!!!   Format is like "New card x y" where x - card color, y - card figure
+		        if(text.startsWith("New card"))
+		        {
+		        	String color = String.valueOf(text.charAt(9));
+		        	String figure = text.substring(11);	
+		        	client.takeNewCard(color, figure);
+		        }
+			}      
+    }
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			listenSocket();
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("Unexpected end");
+		}
+		
+	}
+	
+	protected void finalize() {
+        try 
+        {
+          in.close();
+          out.close();
+        } 
+        catch (IOException e) 
+        {
+          System.out.println("Could not close.");// System.exit(-1);
+        }
+    }
+}
