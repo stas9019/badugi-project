bd8e82bc2a2f2cda2d9cb75cbdc3e3ea943c2862
staszamana95@gmail.com <staszamana95@gmail.com@f38eb0c4-25cb-76d6-1da2-59e377c92437>
2014-11-02 00:39:02 +0000

    Little patch. Added color for cards and text resized.

--- trunk/badugi-project/badugi/src/Client/GameFrame2.java
+++ trunk/badugi-project/badugi/src/Client/GameFrame2.java
@@ -1,5 +1,6 @@
 package Client;
 import java.awt.Color;
+import java.awt.Font;
 //import java.awt.Graphics;							// revision 30
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -213,9 +214,11 @@ public class GameFrame2 extends JFrame implements ActionListener
 		lMoney.setText("  Your money: " + playerMoney);
 	}
 	
-	protected void setCardView(int cardNum, String view)
+	protected void setCardView(int cardNum, String view, Color color)
 	{
 		cards[cardNum].setText(view);
+		cards[cardNum].setFont(new Font(null, Font.PLAIN, 22));
+		cards[cardNum].setForeground(color);
 	}
 	
 	protected void setCurrentPot(int currentPot)//revision 30
